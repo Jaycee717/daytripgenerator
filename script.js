30271beaf8e2c727684331d8127c99fb2e4d0b5c
Susana Matias <susanamatias@Susanas-MacBook-Air.local>
2021-10-25 17:23:19 -0400

    Prompt is popping up but still figuring things out

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
 "use strict"
 
-let randomDestination = ['Washington D.C.', 'Baltimore MD', 'Jacksonville FL','Lancaster PA, Toronto ON'];
+let randomDestination = ['Washington D.C.', 'Baltimore MD', 'Jacksonville FL','Lancaster PA', 'Toronto ON'];
 let randomRestaurant = ['Buffalo Wild Wings', 'Cheddars', 'Mcdonalds','Hibachi', 'Old San Juan'];
 let randomTransportation = ['Plane','Car','Bike','Running','InstantTrnasmission']
 let randomEntertainment = ['Movie','Paintball','Concert','Bon Fire','Stay Inside']
 
 function randomlyPickDestination(finalDestination){
     let chosenDestination = Math.floor(Math.random() * 
-    finalDestinationAnswer.length);
+    randomDestination.length);
     if(chosenDestination === 0){
-        finalDestination = finalDestinationAnswer[0];
+        finalDestination = randomDestination[0];
     } else if(chosenDestination === 1){
-        finalDestination = finalDestinationAnswer[1];
+        finalDestination = randomDestination[1];
     } else if(chosenDestination === 2){
-        finalDestination = finalDestinationAnswer[2];
+        finalDestination = randomDestination[2];
     } else if(chosenDestination === 3){
-        finalDestination = finalDestinationAnswer[3];
+        finalDestination = randomDestination[3];
     } else if(chosenDestination === 4){
-        finalDestination = finalDestinationAnswer[4];
+        finalDestination = randomDestination[4];
     }
     return finalDestination
 }
 
 function randomlyPickRestaurant(finalRestaurant){
     let chosenRestaurant = Math.floor(Math.random() * 
-    finalResaturantAnswer.length);
+    randomRestaurant.length);
     if(chosenRestaurant === 0){
-        finalRestaurant = finalRestaurantAnswer[0];
+        finalRestaurant = randomRestaurant[0];
     } else if(chosenRestaurant === 1){
-        finalRestaurant = finalRestaurantAnswer[1];
+        finalRestaurant = randomRestaurant[1];
     } else if(chosenRestaurant === 2){
-        finalRestaurant = finalRestaurantAnswer[2];
+        finalRestaurant = randomRestaurant[2];
     } else if(chosenRestaurant === 3){
-        finalRestaurant = finalRestaurantAnswer[3];
+        finalRestaurant = randomRestaurant[3];
     } else if(chosenRestaurant === 4){
-        finalRestaurant = finalResaturantAnswer[4];
+        finalRestaurant = randomRestaurant[4];
     }
     return finalRestaurant
 }
 
 function randomlyPickTransportation(finalTransportation){
     let chosenTransportation = Math.floor(Math.random() * 
-    finalTransportationAnswer.length);
+    randomTransportation.length);
     if(chosenTransportation === 0){
-        finalTransportation = finalTransportationAnswer[0];
-    } else if(chosenDestination === 1){
-        finalTransportation = finalTransportationAnswer[1];
-    } else if(chosenDestination === 2){
-        finalTransportation = finalTransportationAnswer[2];
-    } else if(chosenDestination === 3){
-        finalTransportation = finalTransportationAnswer[3];
-    } else if(chosenDestination === 4){
-        finalTransportation = finalTransportationAnswer[4];
+        finalTransportation = randomTransportation[0];
+    } else if(chosenTransportation === 1){
+        finalTransportation = randomTransportation[1];
+    } else if(chosenTransportation === 2){
+        finalTransportation = randomTransportation[2];
+    } else if(chosenTransportation === 3){
+        finalTransportation = randomTransportation[3];
+    } else if(chosenTransportation === 4){
+        finalTransportation = randomTransportation[4];
     }
     return finalTransportation
 }
 
 function randomlyPickEntertainment(finalEntertainment){
     let chosenEntertainment = Math.floor(Math.random() * 
-    finalEntertainmentAnswer.length);
+    randomEntertainment.length);
     if(chosenEntertainment === 0){
-        finalEntertainment = finalEntertainmentAnswer[0];
+        finalEntertainment = randomEntertainment[0];
     } else if(chosenEntertainment === 1){
-        finalEntertainment = finalEntertainmentAnswer[1];
+        finalEntertainment = randomEntertainment[1];
     } else if(chosenEntertainment === 2){
-        finalEntertainment = finalEntertainmentAnswer[2];
+        finalEntertainment = randomEntertainment[2];
     } else if(chosenEntertainment === 3){
-        finalEntertainment = finalEntertainmentAnswer[3];
+        finalEntertainment = randomEntertainment[3];
     } else if(chosenEntertainment === 4){
-        finalEntertainment = finalEntertainmentAnswer[4];
+        finalEntertainment = randomEntertainment[4];
     }
     return finalEntertainment
 }
@@ -81,5 +81,5 @@ let entertainmentAnswer= 'Entertainment' + ' ' + randomlyPickEntertainment()
 
 alert('Your Trip Details \n \n' + destinationAnswer + '\n' + restaurantAnswer + '\n' + transportationAnswer + '\n' + entertainmentAnswer);
 
-let human = prompt('Please type yes, or no.') 
-console.log(prompt) 
+let myName = window.prompt('Are you satisfied')
+console.log("hello", myName)
